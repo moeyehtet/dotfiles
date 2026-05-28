aa469c88813f3f968d601673590eb12da5bb3f81
Moe Ye Htet <moeyehtet96@gmail.com>
2026-05-28 16:49:28 +0200

    turn off nvim regex-based syntax highlighting

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -11,7 +11,10 @@ return {
           "regex", "rust", "scss", "sql", "toml", "tsx", "typescript",
           "vim", "vimdoc", "xml", "yaml",
         },
-        highlight = { enable = true },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
         indent = { enable = true },
       })
     end,
